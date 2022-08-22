99652e56538b0f7351153b8ff668d35a29d4c5e2
Steven Worick <55435176+Stevenworick@users.noreply.github.com>
2022-08-22 23:32:30 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,360 @@
+﻿
+namespace ProjetAssurancePortefeuille
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint1 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(0D, 0D);
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Rebalance_Frequency = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 186);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Time rebalancing";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 240);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Investment";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 292);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Maturity";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(635, 63);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 17);
+            this.label4.TabIndex = 3;
+            // 
+            // Rebalance_Frequency
+            // 
+            this.Rebalance_Frequency.FormattingEnabled = true;
+            this.Rebalance_Frequency.Items.AddRange(new object[] {
+            "DAILY",
+            "WEEKLY",
+            "MONTHLY",
+            "YEARLY"});
+            this.Rebalance_Frequency.Location = new System.Drawing.Point(19, 205);
+            this.Rebalance_Frequency.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Rebalance_Frequency.Name = "Rebalance_Frequency";
+            this.Rebalance_Frequency.Size = new System.Drawing.Size(175, 24);
+            this.Rebalance_Frequency.TabIndex = 4;
+            this.Rebalance_Frequency.SelectedIndexChanged += new System.EventHandler(this.Rebalance_Frequency_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(20, 259);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(19, 311);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(101, 22);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 20F);
+            this.label5.Location = new System.Drawing.Point(202, 217);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 46);
+            this.label5.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 20F);
+            this.label6.Location = new System.Drawing.Point(358, 157);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 46);
+            this.label6.TabIndex = 9;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 20F);
+            this.label7.Location = new System.Drawing.Point(615, 157);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 46);
+            this.label7.TabIndex = 10;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold);
+            this.label8.Location = new System.Drawing.Point(517, 9);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(328, 46);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Portfolio Insurance";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(20, 133);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(83, 17);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "Multiplicator";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(19, 152);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(176, 22);
+            this.textBox4.TabIndex = 13;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(20, 99);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(176, 22);
+            this.textBox5.TabIndex = 14;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(17, 80);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(157, 17);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "Performance Constraint";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 450);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(254, 57);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Launch";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea1);
+            this.chart.Location = new System.Drawing.Point(289, 80);
+            this.chart.Name = "chart";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Name = "Series1";
+            series1.Points.Add(dataPoint1);
+            this.chart.Series.Add(series1);
+            this.chart.Size = new System.Drawing.Size(775, 463);
+            this.chart.TabIndex = 24;
+            this.chart.Text = "chart1";
+            title1.Name = "Title1";
+            title1.Text = "Portfolio\'s Performance ";
+            this.chart.Titles.Add(title1);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "CPPI",
+            "TPPI",
+            "Relative Drawdown"});
+            this.comboBox1.Location = new System.Drawing.Point(19, 366);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 25;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(16, 346);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(143, 17);
+            this.label14.TabIndex = 26;
+            this.label14.Text = "Choose your strategy";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
+            this.label11.Location = new System.Drawing.Point(20, 23);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(261, 32);
+            this.label11.TabIndex = 27;
+            this.label11.Text = "Strategy\'s Parameters";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(37, 570);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(97, 17);
+            this.label12.TabIndex = 28;
+            this.label12.Text = "Historical VaR";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(17, 620);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(138, 17);
+            this.label13.TabIndex = 29;
+            this.label13.Text = "Overall Performance";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(172, 565);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(139, 22);
+            this.textBox3.TabIndex = 30;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(172, 615);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(139, 22);
+            this.textBox6.TabIndex = 31;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1109, 671);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.chart);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Rebalance_Frequency);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Menu";
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox Rebalance_Frequency;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetAssurancePortefeuille
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Rebalance_Frequency_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var Donnee = Utility.GetDatas(Rebalance_Frequency.SelectedItem.ToString());
+            var dataAsset1 = Donnee.Where(x => x.Asset == "1").ToList();
+            var dataAsset2 = Donnee.Where(x => x.Asset == "2").ToList();
+            //var gbm = Utility.gbm(Convert.ToDouble(dataAsset1[0].Price), 5, 200, 0.5, 0.015, 10, 100, true); 
+
+            
+            //Calcul Returns
+            Utility.calcul_returns(dataAsset2);
+            Utility.calcul_returns(dataAsset1);
+          
+            var investment = textBox1.Text!=""  ? Convert.ToDouble(textBox1.Text) : 0;
+            var maturity = textBox2.Text != "" ? Convert.ToDouble(textBox2.Text) : 0;
+        
+            var multiplicator = textBox4.Text != "" ? Convert.ToDouble(textBox4.Text) : 0;
+            var perfConstr = textBox5.Text != "" ? Convert.ToDouble(textBox5.Text) : 0;
+            
+            //Strategies
+            if(comboBox1.SelectedItem.ToString()== "Relative Drawdown")
+            {
+                var resultat = new Strategie.RD(dataAsset1, dataAsset2, investment * perfConstr, 0, perfConstr, investment, multiplicator, maturity, Rebalance_Frequency.SelectedItem.ToString());
+                resultat.weights();
+            }
+            else if (comboBox1.SelectedItem.ToString() == "CPPI")
+            {
+                var resultat = new Strategie.CPPI(dataAsset1, dataAsset2, investment * perfConstr, 0, perfConstr, investment, multiplicator, maturity, Rebalance_Frequency.SelectedItem.ToString());
+                resultat.weights();
+            }
+            else
+            {
+                var resultat = new Strategie.TIPP(dataAsset1, dataAsset2, investment * perfConstr, 0, perfConstr, investment, multiplicator, maturity, Rebalance_Frequency.SelectedItem.ToString());
+                resultat.weights();
+            }
+            chart.ChartAreas[0].AxisY.Maximum = dataAsset1.Where(x => x.Portfolio_Value != 0).Select(x => Math.Round(x.Portfolio_Value, 0)).ToList().Max() + 10;
+            chart.ChartAreas[0].AxisY.Minimum = dataAsset1.Where(x => x.Portfolio_Value != 0).Select(x => Math.Round(x.Portfolio_Value, 0)).ToList().Min() -10;
+            chart.Series[0].Points.DataBindXY(dataAsset1.Where(x=>x.Portfolio_Value!=0).Select(x=>x.Date.ToShortDateString()).ToList(), dataAsset1.Where(x => x.Portfolio_Value != 0).Select(x => Math.Round(x.Portfolio_Value,2)).ToList());
+
+            var returns = dataAsset1.Where(x => x.Portfolio_Value != 0).Select(x => x.Returns).ToList();
+            var prix = dataAsset1.Where(x => x.Portfolio_Value != 0).ToList();
+            Utility.calcul_returns(prix,true);
+            var rendement= prix.Where(x => x.Portfolio_Value != 0).Select(x => x.Returns).ToList();
+            var overall = (prix[prix.Count-1].Portfolio_Value / prix[0].Portfolio_Value) - 1 ;
+            var Rendement_order = prix.OrderBy(x => x.Returns).ToList();
+            var VaR = Rendement_order[Convert.ToInt32(Rendement_order.Count * 0.05)].Returns;
+            textBox3.Text = Convert.ToString(Math.Round(VaR * 100, 2)) + " %";
+            textBox6.Text = Convert.ToString(Math.Round(overall * 100, 2)) + " % "; 
+            
+
+        }
+
+    }
+}
